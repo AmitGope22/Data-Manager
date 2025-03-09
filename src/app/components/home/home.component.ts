8f868a1f752cc34db3d3993df0f2e85125a5fbca
AmitGope22 <103867506+AmitGope22@users.noreply.github.com>
2025-03-09 11:12:56 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,20 +2,23 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DocumentService } from 'src/app/document.service';
 import { AuthService } from 'src/app/services/auth.service';
-
-  
-
+import { Chart, registerables } from 'chart.js'
+import { HttpClient } from '@angular/common/http';
+Chart.register(...registerables);
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  data:any;
   files: any[] = [];
+  dataname: any[] = [];
+  datasize: any[] = [];
   totalFiles: number = 0;
   totalUsers: number = 0;
   uploadCount: number = 0;
-  constructor( private _auth: AuthService, private router:Router,private fileService:DocumentService) { }
+  constructor( private _auth: AuthService, private router:Router,private fileService:DocumentService,private http: HttpClient) { }
 
   ngOnInit(): void {
   
@@ -29,7 +32,23 @@ export class HomeComponent {
     this.getUploadCount();
   }
   {
-    this._auth.getUserForToday().subscribe(
+    this.http.get<any[]>('http://localhost:4000/file//files').subscribe(res=>{
+      this.data=res;
+      if(this.data!=null)
+      {
+        for(let i=0;i<this.data.length;i++)
+        {
+          this.dataname.push(this.data[i].originalfile);
+          this.datasize.push(this.data[i].fileSize);
+          
+        }
+  
+      }
+      this.showchatdata(this.dataname,this.datasize);
+    })
+  }
+  {
+    this.fileService.getFilesForToday().subscribe(
       (files) => {
         this.files = files;
       },
@@ -39,6 +58,25 @@ export class HomeComponent {
     );
   }
 
+}
+showchatdata(dataname:any, datasize:any){
+  const myChart=new Chart("myChart", {
+    type: 'bar',
+    data: {
+      labels: dataname,
+      datasets: [{
+        label: '# of Size',
+        data: datasize,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      
+        indexAxis: 'y',
+        
+      
+    }
+  });
 }
   loadFileCounts(): void {
     this.fileService.getFileCounts().subscribe(data => {
@@ -63,8 +101,3 @@ export class HomeComponent {
   }
 
 }
-
-
- 
-  
-
